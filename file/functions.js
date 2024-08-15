86afc7dee15059387715aa2f49be4cfcd435020d
AshenOne-xd <gamersaitamabuisness@gmail.com>
2024-08-15 13:27:03 +0530

    Update functions.js

--- file/functions.js
+++ file/functions.js
@@ -55,6 +55,6 @@ function timeElapse(date){
 	if (seconds < 10) {
 		seconds = "0" + seconds;
 	}
-var result = "days <span class=\"digit\">" + days + "</span> hours <span class=\"digit\">" + hours + "</span> minutes <span class=\"digit\">" + minutes + "</span> seconds <span class=\"digit\">" + seconds + "</span> 秒"; 
+var result = "days <span class=\"digit\">" + days + "</span> hours <span class=\"digit\">" + hours + "</span> minutes <span class=\"digit\">" + minutes + "</span> seconds <span class=\"digit\">" +    seconds + "</span> "; 
 	$("#clock").html(result);
 }
